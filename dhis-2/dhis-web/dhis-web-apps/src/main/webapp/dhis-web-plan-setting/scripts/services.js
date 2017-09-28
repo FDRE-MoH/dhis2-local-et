22b360eaa3ea0d4b049a9eb56c9c89b88bc590a2
abyot <abyota@gmail.com>
2017-09-28 09:40:52 +0200

    minor

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-plan-setting/scripts/services.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-plan-setting/scripts/services.js
@@ -43,14 +43,14 @@ var planSettingServices = angular.module('planSettingServices', ['ngResource'])
         angular.forEach(d2Periods, function(p){
             p.id = p.iso;
             var st = p.endDate.split('-');
-            st[1] = dhis2.routineDataEntry.monthNames[calendarSetting.keyCalendar].indexOf( st[1] ) + 1;
+            st[1] = dhis2.planSetting.monthNames[calendarSetting.keyCalendar].indexOf( st[1] ) + 1;
             if( st[1] < 10 ){
                 st[1] = '0' + st[1];
             }
             p.endDate = st.join('-');
             
             st = p.startDate.split('-');
-            st[1] = dhis2.routineDataEntry.monthNames[calendarSetting.keyCalendar].indexOf( st[1] ) + 1;
+            st[1] = dhis2.planSetting.monthNames[calendarSetting.keyCalendar].indexOf( st[1] ) + 1;
             if( st[1] < 10 ){
                 st[1] = '0' + st[1];
             }
